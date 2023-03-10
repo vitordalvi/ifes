e6ba46802ce0a76dbe0c10ba5ea84214c632b29b
Vitor Dalvi <114376105+vitordalvi@users.noreply.github.com>
2023-03-10 00:39:26 -0300

    Update MMC.java

--- me.vitor.dalvi.mmc/src/MMC.java
+++ me.vitor.dalvi.mmc/src/MMC.java
@@ -6,34 +6,42 @@ public class MMC {
 
         char opcao = 's';
 
+        do {
             System.out.println();
-            System.out.println("Para calcular o MMC dos números\nselecionados, digite qualquer letra ou número.");
-            String a = sc.nextLine();
-        while (opcao == 's') {
-            System.out.println();
-            System.out.print("Digite o primeiro número: ");
-            int num1 = sc.nextInt();
-            System.out.print("Digite o segundo número: ");
-            int num2 = sc.nextInt();
+            System.out.print("Escolha a quantidade de números que você deseja calcular: ");
+            int g = sc.nextInt();
+            int[] num = new int[g];
 
-            int mmc = calcularMMC(num1, num2);
+            for (int i = 0; i < g; i++) {
+                System.out.print("Digite o #" + (i + 1) + " número: ");
+                num[i] = sc.nextInt();
+            }
 
-            System.out.println("O MMC de " + num1 + " e " + num2 + " é: " + mmc);
-            System.out.println("");
+            int totalMMC = num[0];
+            for (int i = 1; i < g; i++) {
+                totalMMC = calcularMMC(totalMMC, num[i]);
+            }
+
+            System.out.println("O MMC total dos números é: " + totalMMC);
             System.out.print("Você deseja calcular mais números? (S/N): ");
             opcao = Character.toLowerCase(sc.next().charAt(0));
-        }
+        } while (opcao == 's');
     }
 
     private static int calcularMMC(int num1, int num2) {
         int a = num1;
         int b = num2;
+
         while (b != 0) {
             int res = a % b;
             a = b;
             b = res;
         }
-        int mmc = (num1 * num2) / a;
+
+        int mmc = 0;
+        if (a != 0) {
+            mmc = (num1 * num2) / a;
+        }
 
         return mmc;
     }
