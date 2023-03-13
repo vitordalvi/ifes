93e8b066ce087eb3b353141a148fbe0847acb67d
Vitor Dalvi <114376105+vitordalvi@users.noreply.github.com>
2023-03-13 00:54:18 -0300

    Update MMC.java

--- me.vitor.dalvi.mmc/src/MMC.java
+++ me.vitor.dalvi.mmc/src/MMC.java
@@ -18,31 +18,30 @@ public class MMC {
             }
 
             int totalMMC = num[0];
+            int[][] contaMMC = new int[g - 1][4];
             for (int i = 1; i < g; i++) {
-                totalMMC = calcularMMC(totalMMC, num[i]);
+                int a = totalMMC;
+                int b = num[i];
+                while (b != 0) {
+                    int res = a % b;
+                    contaMMC[i - 1][0] = a;
+                    contaMMC[i - 1][1] = b;
+                    contaMMC[i - 1][2] = res;
+                    a = b;
+                    b = res;
+                }
+                totalMMC = (totalMMC * num[i]) / a;
+                contaMMC[i - 1][3] = totalMMC;
             }
 
             System.out.println("O MMC total dos números é: " + totalMMC);
+            System.out.println("Resultado das contas:");
+            for (int i = 0; i < g - 1; i++) {
+                System.out.printf("%d | %d | %d | %d\n", contaMMC[i][0], contaMMC[i][1], contaMMC[i][2], contaMMC[i][3]);
+            }
+
             System.out.print("Você deseja calcular mais números? (S/N): ");
             opcao = Character.toLowerCase(sc.next().charAt(0));
         } while (opcao == 's');
-    }
-
-    private static int calcularMMC(int num1, int num2) {
-        int a = num1;
-        int b = num2;
-
-        while (b != 0) {
-            int res = a % b;
-            a = b;
-            b = res;
-        }
-
-        int mmc = 0;
-        if (a != 0) {
-            mmc = (num1 * num2) / a;
-        }
-
-        return mmc;
     }
 }
